b3544946d5f70a989dc252f226334317c1ca8f34
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-28 13:43:36 +0200

    commit

--- app/src/main/java/com/example/dice/viewmodel/scope/MainActivity.kt
+++ app/src/main/java/com/example/dice/viewmodel/scope/MainActivity.kt
@@ -3,6 +3,16 @@ package com.example.dice.viewmodel.scope
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+val diceImages: List<Int> by lazy {
+    listOf(
+        R.drawable.dice_face_one,
+        R.drawable.dice_face_two,
+        R.drawable.dice_face_three,
+        R.drawable.dice_face_four,
+        R.drawable.dice_face_five,
+        R.drawable.dice_face_six,
+    )
+}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
