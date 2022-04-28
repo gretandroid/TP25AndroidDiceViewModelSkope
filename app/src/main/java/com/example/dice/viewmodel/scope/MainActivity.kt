a24569dfbee700796c582f717162e9e2625ce3ec
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-28 14:06:56 +0200

    commit

--- app/src/main/java/com/example/dice/viewmodel/scope/MainActivity.kt
+++ app/src/main/java/com/example/dice/viewmodel/scope/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.dice.viewmodel.scope
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.lifecycle.*
+import com.example.dice.viewmodel.scope.databinding.ActivityMainBinding
 
 val diceImages: List<Int> by lazy {
     listOf(
@@ -13,9 +16,26 @@ val diceImages: List<Int> by lazy {
         R.drawable.dice_face_six,
     )
 }
+
+class DicesViewModel : ViewModel() {
+    private val _dicesThrow = MutableLiveData<Pair<Int, Int>>()
+    val dicesThrow: LiveData<Pair<Int, Int>> = _dicesThrow
+
+}
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val images = listOf(
+            binding.firstDiceImage,
+            binding.secondDiceImage,
+            binding.thirdDiceImage,
+        )
+        val dicesViewModel: DicesViewModel = ViewModelProvider(this).get(DicesViewModel::class.java)
+
+
+        setContentView(binding.root)
     }
 }
